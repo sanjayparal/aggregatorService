16be01583edc03765d2b843c5cd3ecbadebe3853
sanjayparal <sanjay.paral@nagarro.com>
2020-07-06 14:57:04 +0530

    Commiting all the changes

---
+++ src/main/java/com/nagarro/demo/model/Order.java
@@ -0,0 +1,32 @@
+package com.nagarro.demo.model;
+
+public class Order {
+
+    private Integer orderId;
+    private Integer orderAmount;
+    private String orderDate;
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public Integer getOrderAmount() {
+        return orderAmount;
+    }
+
+    public void setOrderAmount(Integer orderAmount) {
+        this.orderAmount = orderAmount;
+    }
+
+    public String getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(String orderDate) {
+        this.orderDate = orderDate;
+    }
+}
